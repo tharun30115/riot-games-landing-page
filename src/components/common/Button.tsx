967047a031fc900cb0ccc1bcb76427936cdf0f6f
Tharun <tharungowda30115@gmail.com>
2025-03-04 22:47:10 +0530

    Quick commit (No specific file changes detected)

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,9 +2,9 @@ const Button = (props: any) => {
   const { buttonTitle } = props;
 
   return (
-    // <button className="bg-[#d13639] px-6 py-2 rounded-full text-[12px] text-black font-bold tracking-wider">
-    //   {buttonTitle}
-    // </button>
+    <button className="bg-[#d13639] px-6 py-2 rounded-full text-[12px] text-black font-bold tracking-wider">
+      {buttonTitle}
+    </button>
   );
 };
 
