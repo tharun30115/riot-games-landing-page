72cd2818270a474c8377bd99734d4434f9ebc7c4
Tharun <tharungowda30115@gmail.com>
2025-03-04 22:46:03 +0530

    Quick commit (No specific file changes detected)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,13 @@ const Hero = () => {
           className="absolute inset-0 object-cover w-full h-[90%] -z-10 brightness-50"
         />
 
-        <div className="py-32 pl-12 mt-6 sm:py-48 lg:py-56">
+        {/* <div className="py-32 pl-12 mt-6 sm:py-48 lg:py-56">
           <div className="flex gap-11 flex-col items-center text-white w-[40rem]">
             <img
               src="https://www.riotgames.com/darkroom/500/ca793edf399904c3dc1fe954699d7d02:8d4ba21708fdcbe732355d6585a0400d/convergence-logo-riotgames-600x121.png"
               alt="Logo"
               className="w-[22.5rem]"
-            />
+            /> */}
 
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="text-3xl font-bold">
