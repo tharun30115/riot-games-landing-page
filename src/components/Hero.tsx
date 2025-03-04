e53d67ba1816a842693b10b28550a2db341b7d08
Tharun <tharungowda30115@gmail.com>
2025-03-04 22:45:50 +0530

    Quick commit (No specific file changes detected)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,27 +28,7 @@ const Hero = () => {
               </p>
             </div>
 
-            <button className="flex items-center justify-center gap-4 bg-[#d53235] py-3 px-4 rounded-3xl hover:shadow-2xl transition ease-out hover:shadow-[#d53235]">
-              <div className="relative flex items-center justify-center">
-                <div className="w-12 h-12 p-2 bg-black rounded-full"></div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={3.5}
-                  stroke="currentColor"
-                  className="absolute w-6 h-6 right-3"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                  />
-                </svg>
-              </div>
-
-              <p className="text-3xl font-bold tracking-wider">Available Now</p>
-            </button>
+           
           </div>
         </div>
       </div>
