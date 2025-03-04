79d57bfc805ec36823f8186ca7f7fc36aed5dc38
Tharun <tharungowda30115@gmail.com>
2025-03-04 22:45:21 +0530

    Quick commit (No specific file changes detected)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -21,6 +21,31 @@ const NavBar = () => {
   };
 
   return (
+    <nav className="absolute top-0 z-10 flex items-start justify-between pt-7 px-8 w-[100%] text-white">
+      <div className="flex items-center gap-2 left-nav">
+        <div className="flex mr-8 gap-7">
+          <Logo
+            logoColor={logoColor}
+            handleMouseEnter={handleMouseEnter}
+            handleMouseLeave={handleMouseLeave}
+          />
+
+          <Punch />
+        </div>
+
+        {textMenus.map((menu) => (
+          <div className="text-menus">{menu.name}</div>
+        ))}
+      </div>
+
+      <div className="flex items-center gap-2 text-white right-nav font-primary">
+        <Globe />
+
+        <Search />
+
+        <Button buttonTitle="SIGN IN" />
+      </div>
+    </nav>
   );
 };
 
